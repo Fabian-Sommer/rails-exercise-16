0a24bb51ed9ca6af8111862e9962b474bbe88714
Fabian Sommer <fabiansbg@web.de>
2016-10-27 19:39:24 +0200

    Closes #13.

--- spec/views/authors/new.html.erb_spec.rb
+++ spec/views/authors/new.html.erb_spec.rb
@@ -24,6 +24,6 @@ describe "New author page", :type => :feature do
 
   it "should have a create author button" do
 	visit 'new_author'
-	expect(page).to have_button("Save Author")
+	expect(page).to have_button("Create Author")
   end
 end
