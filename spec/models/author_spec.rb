54fa56a447d11efe70b8efc37ee0ca2b8ab07598
Fabian Sommer <fabiansbg@web.de>
2016-10-27 20:29:41 +0200

    Failing test case

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Author, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
+  describe "Given an author a with first name 'Alan' and last name 'Turing'" do
+	it "then a.name returns 'Alan Turing'" do
+		author = Author.create!
+		author.first_name = "Alan"
+		author.last_name = "Turing"
+		expect(author.name).to eq("Alan Turing")
+	end
+  end
 end
