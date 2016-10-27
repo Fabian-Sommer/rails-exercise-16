1a4730a64c3e05019aca4ecc13642db3043151cf
Fabian Sommer <fabiansbg@web.de>
2016-10-27 13:51:28 +0200

    Additional route for creating author. Closes #8

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'authors' => 'authors#index'
   get '/authors/:id(.:format)' => 'authors#show'
   get 'author_new' => 'authors#new'
+  get 'new_author' => 'authors#new'
   resources :authors
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
