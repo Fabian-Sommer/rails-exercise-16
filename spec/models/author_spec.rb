0537b3837babe5f92bad7ea0a16ee24d61083552
Fabian Sommer <fabiansbg@web.de>
2016-10-29 12:44:42 +0200

    Failing test case

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,10 +4,18 @@ RSpec.describe Author, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
   describe "Given an author a with first name 'Alan' and last name 'Turing'" do
 	it "then a.name returns 'Alan Turing'" do
-		author = Author.create!
-		author.first_name = "Alan"
-		author.last_name = "Turing"
+		author = Author.create(first_name: "Alan", last_name: "Turing")
 		expect(author.name).to eq("Alan Turing")
 	end
   end
+  
+  describe "An author" do
+	it "should always have a first name" do
+		expect(Author.create(first_name: nil).valid?).to eq(false)
+	end
+	it "should always have a last name" do
+		expect(Author.create(last_name: nil).valid?).to eq(false)
+	end
+  end
+  
 end
