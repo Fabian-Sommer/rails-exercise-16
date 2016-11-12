ae2eee778e41573941c6da950d1d4ad29aa1a376
Fabian Sommer <fabiansbg@web.de>
2016-11-12 13:04:31 +0100

    renamed more things from name to title. Closes #28.

--- db/migrate/20161029152349_create_papers.rb
+++ db/migrate/20161029152349_create_papers.rb
@@ -1,7 +1,7 @@
 class CreatePapers < ActiveRecord::Migration
   def change
     create_table :papers do |t|
-      t.string :name
+      t.string :title
       t.string :venue
       t.integer :year
 
